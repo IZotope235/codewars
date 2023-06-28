0aa4b04010c04c4f8b83875c42eb86a0dfa66827
iZotope235 <mmnik84@mail.ru>
2023-06-28 11:23:45 +0300

    Create a function with two arguments that will return an array of the first n multiples of x.
    Assume both the given number and the number of times to count will be positive numbers greater than 0.
    
    Return the results as an array or list ( depending on language ).

--- src/main/java/codewars/CountBy.java
+++ src/main/java/codewars/CountBy.java
@@ -1,4 +1,19 @@
 package codewars;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class CountBy {
+
+    public static int[] countBy(int x, int n) {
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = x * (i + 1);
+        }
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBy(1, 10)));
+    }
 }
